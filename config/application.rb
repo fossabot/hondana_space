1b4b5f40bfc29c265a7ed47c10df03029aa4d70b
tetsuya <tetsuya@users.noreply.github.com>
2017-06-14 19:48:15 +0900

    Initial setup
    Modify .gitignore
    $ rbenv local 2.4.1
    Modify Gemfile
    $ bundle install
    $ be rails g annotate:install
    $ be rails g rspec:install
    $ be spring binstub --all
    Modify .rubocop.yml
    $ be rubocop -a
    Modify .env
    $ be rails db:create; and be rails db:migrate

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,22 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module HondanaSpace
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |generate|
+      generate.helper false
+      generate.javascripts false
+      generate.request_specs false
+      generate.routing_specs false
+      generate.stylesheets false
+      generate.test_framework :rspec
+      generate.view_specs false
+    end
+    config.i18n.default_locale = :ja
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
   end
 end
