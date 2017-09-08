fb1f410bcf700be34b64a400a8c103713f3404c8
tetsuya <tetsuya@users.noreply.github.com>
2017-09-09 00:40:06 +0900

    Herok ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root   "statics#home"
+  root   "books#index"
   get    "/books"            => "books#index", as: :books
   get    "/books/new"        => "books#new", as: :new_book
   post   "/books"            => "books#create"
